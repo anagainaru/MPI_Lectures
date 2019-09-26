79cd98b81766a48a6bafc4cb4d69881cc82142b7
Ana Gainaru <ana.gainaru@gmail.com>
2019-09-26 11:59:00 -0400

    measuring time example

---
+++ code_python/9_wtime.py
@@ -0,0 +1,18 @@
+from mpi4py import MPI
+import numpy as np
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+comm.barrier();
+local_wt = MPI.Wtime();
+
+data = np.array([rank * i for i in range(10)])
+comm.Bcast(data, root=0); 
+local_wt = MPI.Wtime() - local_wt;
+
+wt = comm.reduce(local_wt, op=MPI.MAX, root=0);
+
+if rank == 0:
+    print("Bcast time %f" %(wt))
